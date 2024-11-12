5748b8e8494651bfd02bc428c2c0710ace527f89
Alejandro Alzate Zapata <aalzate111@gmail.com>
2024-11-11 22:08:20 -0500

    Agregar vista del detalle del producto

---
+++ Meli/Presentation/Dashboard/DashboardView.swift
@@ -0,0 +1,60 @@
+//
+//  DashboardView.swift
+//  Meli
+//
+//  Created by Alejandro Alzate Zapata on 28/10/24.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    @StateObject var viewModel: DashboardViewModel
+    @State private var searchText = ""
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                TextField("Buscar...", text: $searchText)
+                    .padding(8)
+                    .background(Color.gray.secondary)
+                    .padding(.horizontal)
+                    .onChange(of: searchText) { oldValue, newValue in
+                        searchText = newValue
+                        viewModel.searchProducts(text: newValue)
+                    }
+                
+                if let products = viewModel.products?.enumerated().map({ $0 }) {
+                    List(products, id: \.element.id) { index, product in
+                        NavigationLink(destination: DetailView(product: product)) {
+                            ProductRow(product: product)
+                                .onAppear {
+                                    viewModel.fetchMoreProductIfNeeded(text: searchText, index: index + 1)
+                                }
+                        }                        
+                    }.overlay(content: {
+                        if viewModel.isLoading {
+                            ProgressView()
+                        }
+                    })
+                    .listStyle(.plain)
+                }
+                
+            }
+            .padding()
+            .navigationTitle("Buscador")
+        }
+    }
+}
+
+#Preview {
+    DashboardView(
+        viewModel: DashboardViewModel(
+            fetchProductsBySearchUseCase: FetchProductsBySearchUseCase(
+                repository: ProductRepository(
+                    requestBuilder: RequestBuilder(),
+                    client: HTTPClient()
+                )
+            )
+        )
+    )
+}
---
+++ Meli/Presentation/Detail/DetailView.swift
@@ -0,0 +1,83 @@
+//
+//  DetailView.swift
+//  Meli
+//
+//  Created by Alejandro Alzate Zapata on 11/11/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let product: Product
+    
+    var body: some View {
+        VStack {
+            Text(product.title)
+                .font(.title)
+            Divider()
+            let imageUrl = product.thumbnail.replacing("http:", with: "https:")
+            AsyncImage(url: URL(string: imageUrl)) { phase in
+                switch phase {
+                case .success(let image):
+                    image.resizable()
+                case .failure(_):
+                    Color.gray
+                default:
+                    ProgressView()
+                }
+                
+            }
+            .frame(width: 128, height: 128)
+            .clipShape(.rect(cornerRadius: 25))
+            Divider()
+            Text(formatToCOP(product.price))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .font(.title2)
+            Spacer()
+            Button(action: {
+                print("Comprar \(product.title)")
+            }) {
+                Text("Comprar Ahora")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            Button(action: {
+                print("Agregar al carrito \(product.title)")
+            }) {
+                Text("Agregar al carrito")
+                    .font(.headline)
+                    .foregroundColor(Color.blue)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue.secondary)
+                    .cornerRadius(10)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        .padding(20)
+    }
+    
+    private func formatToCOP(_ value: Double) -> String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencyCode = "COP"
+        formatter.maximumFractionDigits = 0
+
+        return formatter.string(from: NSNumber(value: value)) ?? "$0"
+    }
+}
+
+#Preview {
+    DetailView(product: Product(
+        id: "MCO2707035890",
+        title: "Celular Moto G04s 128gb Negro",
+        price: 509639,
+        currencyId: "COP",
+        thumbnail:             "https://http2.mlstatic.com/D_679639-CBT80375284663_112024-I.jpg"
+)
+    )
+}
